5b2e076248d1e00c4f72f65a53de3e493c9a1a1a
fhackett <fhackett.py@gmail.com>
2018-01-17 21:30:22 -0800

    Add some more notes to session4

--- session4/weaklinking/main.cpp
+++ session4/weaklinking/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 int main(){
+    // check if new_fn is present. If we link against a .so file
+    // that doesn't contain that function, the linker will set
+    // the address of new_fn to nullptr.
     if(common::new_fn){
         std::cout << common::new_fn("beep") << "\n";
     }else{
